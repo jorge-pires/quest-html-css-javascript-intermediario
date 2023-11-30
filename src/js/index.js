a15a47f9482b2af83e824a2db7b2f00fe26b388a
Jorge Pires <jorge.business@outlook.com.br>
2023-11-30 19:45:10 -0300

    Otimização

--- src/js/index.js
+++ src/js/index.js
@@ -1,18 +1,15 @@
-let inputPadrao = document.querySelectorAll(".input-item")
-const btnEnviar = document.querySelector(".btn")
-
-console.log(inputPadrao)
-
+let inputPadrao = document.querySelectorAll(".input-item");
+const btnEnviar = document.querySelector(".btn");
 
 btnEnviar.addEventListener('click', function () {
     inputPadrao.forEach(function (input) {
         if (input.children[0].value !== "") {
-            input.classList.remove("desativado");
-            input.classList.add("ativado");
+            input.classList.remove("vazio");
+            input.classList.add("preenchido");
             input.children[1].classList.remove("aviso-ativo");
         } else {
-            input.classList.remove("ativado");
-            input.classList.add("desativado");
+            input.classList.remove("preenchido");
+            input.classList.add("vazio");
             input.children[1].classList.add("aviso-ativo");
         }
     });
